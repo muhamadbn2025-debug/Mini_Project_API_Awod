243d9b247098a8c1b578800fffebfb5d8c980eb0
Muhamad Awod <muhamadbn2025@gmail.com>
2026-03-29 02:11:20 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+const testConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connected");
+    connection.release();
+  } catch (error) { console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = {
+  pool,
+  testConnection,
+};
